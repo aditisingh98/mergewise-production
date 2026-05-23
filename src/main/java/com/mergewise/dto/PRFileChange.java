842542856114a529476eda637050db8d726f6b3a
Aman <aman.bansal@tothenew.com>
2026-05-23 17:31:23 +0530

    Add application properties

---
+++ src/main/java/com/mergewise/dto/PRFileChange.java
@@ -0,0 +1,19 @@
+package com.mergewise.dto;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class PRFileChange {
+ private String filename;
+ private String status;
+ private Integer additions;
+ private Integer deletions;
+ private Integer changes;
+ private String previousFilename;
+ private String patch;
+ private List<String> addedLines = new ArrayList<>();
+ private List<String> removedLines = new ArrayList<>();
+}
